763aa874278ff8a66428cc19ab541cd3235c0434
MohamedMahmoud7Te <152813379+MohamedMahmoud7Te@users.noreply.github.com>
2023-12-06 19:00:58 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// var x =document.getElementById("mohamed");
+// x.innerHTML = "Hallo from external file js";
+// x.style.color = "#f00";
+// x.style.textAlign = "center";
+//camel case
+
+
+//show code js
+
+//1-console
+// console.log("25");
+
+//2-alert
+//  alert("25");
+
+//3-docuoment.write
+// document.write("2555");
+
+//4-innerHTML
+// document.getElementById("mohamed").innerHTML = "255555"
+
+//variable
+
+// let 
+// const
+
+
+
+
+// var x = 5;
+// var y = 10;
+// var z = x + y;
+
+// console.log(z);
+// var x = "ahmed"; //string
+// var y = 25; //number
+// var z = true; //boolean
+// var w;
+// var names = ["ahmed","ali",25,ture];  //arrey
+// console.log(typeof names);  //object
+
+// var person = {
+//     firstName:"ahmed",
+//     lastName:"ali",
+//     age:25,
+//     isAdmen:true
+// }
+
+// console.log(typeof person)
